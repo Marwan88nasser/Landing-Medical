3f587e65c76961134539893ba8ff8a5bd37b6c8e
Marwan88nasser <marwan.nasser565@gmail.com>
2025-09-14 16:00:09 +0300

    last update

---
+++ js/main.js
@@ -0,0 +1,30 @@
+new Swiper(".team-slider", {
+  loop: true,
+  grabCursor: true,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+  },
+});
